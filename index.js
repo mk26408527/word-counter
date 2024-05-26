3a07c61e6cd00e34ef98caa10e636437f07d7218
mk26408527 <mk26408527@gmail.com>
2024-05-26 15:33:14 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bold.italic.bgGreenBright("\n \t\t M_Huaifa - Word Counter"));
+console.log("=".repeat(60));
+let answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: " Enter your sentence to count the word: "
+    }
+]);
+const words = answers.Sentence.trim().split(" ");
+console.log(words);
+console.log(`Your sentence word count is ${words.length}`);
